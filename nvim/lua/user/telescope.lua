08059590315e47f28b7eec3390739c70caec1a02
dan.castillo <dan.castillo@jasper.ai>
2023-10-13 10:33:58 -0400

    telescope search for files in gitignore

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -1,8 +1,21 @@
 -- [[ Configure Telescope ]]
-local actions = require "telescope.actions"
+local actions = require 'telescope.actions'
 
 require('telescope').setup {
   defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--hidden',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case'
+    },
+    file_ignore_patterns = {
+      'node_modules', 'build', 'dist', 'yarn.lock', 'package-lock.json'
+    },
     mappings = {
       i = {
         ['<C-u>'] = false,
@@ -12,6 +25,25 @@ require('telescope').setup {
         ['<esc>'] = actions.close,
         ['q'] = actions.close,
       }
+    },
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+    live_grep = {
+      --@usage don't include the filename in the search results
+      only_sort_text = true,
+    },
+    grep_string = {
+      only_sort_text = true,
+    },
+    git_files = {
+      hidden = true,
+      show_untracked = true,
+    },
+    colorscheme = {
+      enable_preview = true,
     },
   },
 }
