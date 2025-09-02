7451b2b1950d68e6e6635c557d5f7c44c491115e
dan.castillo <dancastillo99@gmail.com>
2025-09-02 19:47:21 -0400

    update config

--- nvim/lua/user/dap.lua
+++ nvim/lua/user/dap.lua
@@ -35,7 +35,7 @@ local M = {
       dependencies = {
         "microsoft/vscode-js-debug",
         version = '1.x',
-        build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+        build = "git reset --hard && npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
       },
       config = function()
         local dap = require "dap"
