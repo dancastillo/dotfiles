a09c5a1ca06e0afbbf7085a98835f54b6b36a936
Dan Castillo <dancastillo99@gmail.com>
2025-09-09 07:50:15 -0400

    update config (#7)

--- nvim/lua/user/indentline.lua
+++ nvim/lua/user/indentline.lua
@@ -1,11 +1,6 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
-    -- main = "ibl",
-    -- ---@module "ibl"
-    -- ---@type ibl.config
-  -- opts = {},
   event = "VeryLazy",
-  -- commit = "9637670896b68805430e2f72cf5d16be5b97a22a",
 }
 
 function M.config()
@@ -27,36 +22,7 @@ function M.config()
     },
     scope = { enabled = false },
     indent = { char = icons.ui.LineMiddle },
-    -- char = icons.ui.LineMiddle,
-    -- context_char = icons.ui.LineMiddle,
-    --
-    -- show_trailing_blankline_indent = false,
-    -- remove_blankline_trail = true,
-
-    -- show_first_indent_level = true,
-    -- show_current_context = true,
-    -- use_treesitter = true,
   }
-
-  -- indent = { char = icons.ui.LineMiddle },
-  -- whitespace = {
-  --   remove_blankline_trail = true,
-  -- },
-  --
-  -- exclude = {
-  --   filetypes = {
-  --     "help",
-  --     "startify",
-  --     "dashboard",
-  --     "lazy",
-  --     "neogitstatus",
-  --     "NvimTree",
-  --     "Trouble",
-  --     "text",
-  --   },
-  --   buftypes = { "terminal", "nofile" },
-  -- },
-  -- scope = { enabled = false },
 end
 
 return M
